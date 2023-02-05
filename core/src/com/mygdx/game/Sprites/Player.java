49487766b64eca94767f138a779354da0b33577d
Ammar Syed <am428097@dal.ca>
2023-02-05 01:52:20 -0400

    Added Entity and basic Player classes

--- core/src/com/mygdx/game/Sprites/Player.java
+++ core/src/com/mygdx/game/Sprites/Player.java
@@ -1,5 +1,26 @@
 package com.mygdx.game.Sprites;
 
-public class Player {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Body;
 
+import static com.mygdx.game.Helper.Constants.PPM;
+
+public class Player extends Entity {
+
+    public Player(float width, float height, Body body) {
+        super(width, height, body);
+        this.speed = 4f;
+
+    }
+
+    @Override
+    public void update() {
+        x = body.getPosition().x * PPM;
+        y = body.getPosition().y * PPM;
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
 }
