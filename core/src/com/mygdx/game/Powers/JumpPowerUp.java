ac0bc5fb4ebf494e472b4de8128285c424163c30
Ammar Syed <am428097@dal.ca>
2023-02-16 20:50:17 -0400

    no commit

--- core/src/com/mygdx/game/Powers/JumpPowerUp.java
+++ core/src/com/mygdx/game/Powers/JumpPowerUp.java
@@ -4,14 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class JumpPowerUp extends PowerUp{
-
     public JumpPowerUp(float x, float y, World world){
         super(x, y, world);
         fixture.setUserData(this);
     }
-
+    
     @Override
     public void powerUpActivate() {
         Gdx.app.log("Jump Power", "Collision");
     }
 }
+
