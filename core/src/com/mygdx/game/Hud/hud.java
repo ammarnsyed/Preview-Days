352a5b07f88c070a569bac182ebc7c0d63d4154c
Hao <Ninahw2016@gmail.com>
2023-03-17 11:59:30 -0300

    hud class created

---
+++ core/src/com/mygdx/game/Hud/hud.java
@@ -0,0 +1,22 @@
+package com.mygdx.game.Hud;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class hud implements Disposable{
+
+
+  @Override
+  public void dispose() {
+
+  }
+}
