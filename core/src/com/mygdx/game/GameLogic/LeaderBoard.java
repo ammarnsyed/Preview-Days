4920a3255598bb8302374cd6d3869a8a74f3eff3
Hao <Ninahw2016@gmail.com>
2023-04-05 11:30:31 -0300

    add leaderboard page

---
+++ core/src/com/mygdx/game/GameLogic/LeaderBoard.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.GameLogic;
+
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class LeaderBoard extends ScreenAdapter{
+
+}
