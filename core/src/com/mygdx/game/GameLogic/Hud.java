a114dac3c6354291928fa6d39312e73c746364e1
Ammar Syed <am428097@dal.ca>
2023-04-10 14:59:34 -0300

    Removed resume functionality from setting button

--- core/src/com/mygdx/game/GameLogic/Hud.java
+++ core/src/com/mygdx/game/GameLogic/Hud.java
@@ -175,7 +175,6 @@ public class Hud implements Disposable{
     button2.addListener(new ChangeListener() {
       @Override
       public void changed(ChangeEvent event, Actor actor) {
-        playScreen.updateResume();
       }
     });
 
